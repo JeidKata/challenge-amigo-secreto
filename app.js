0e146409a7afe9a59e18ed42f4563dfa82d77881
JeidyOlaya <jeidkata@gmail.com>
2025-07-31 18:33:42 -0500

    Feat: Implements a feature to add friends. Capture the value of the input field. Validate the entry. Update the friends array. Clear the input field.

--- app.js
+++ app.js
@@ -1,1 +1,17 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
+let amigos = [];
+
+
+
+function agregarAmigo() {
+    let nombreAmigo = document.getElementById("amigo").value;
+    console.log(amigos);
+    if (nombreAmigo === "") {
+        alert("Por favor, ingresa un nombre.");
+        return;
+    }else{
+        amigos.push(nombreAmigo);
+        alert(`Se agrego el amigo ${nombreAmigo} a la lista.`);
+        document.getElementById("amigo").value = "";
+    }
+}
